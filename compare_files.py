0bb72a5ec035d46ea7021efc35f8e3b3493cf832
Gulshan Kumar <gulshan17@outlook.com>
2023-04-20 23:52:05 +0530

    Update compare_files.py
    added option to exclude columns while matching the files

--- compare_files.py
+++ compare_files.py
@@ -12,12 +12,27 @@ df_join = df1.merge(right = df2,
 df1.rename(columns = lambda x : x + '_file1', inplace = True)
 df2.rename(columns = lambda x : x + '_file2', inplace = True)
 # %%
+df1_columns_to_join = df1.columns.to_list()
+df2_columns_to_join = df2.columns.to_list()
+# %%
+# columns to exclude while joining, mention the columns to exclude while matching the data in comma separated like ['column1', 'column2']
+df1_columns_to_exclude_join = []
+df2_columns_to_exclude_join = []
+# %%
+df1_columns_to_exclude_join = [x if x[-6 : ] == '_file1' else x + '_file1' for x in df1_columns_to_exclude_join]
+
+df2_columns_to_exclude_join = [x if x[-6 : ] == '_file2' else x + '_file2' for x in df2_columns_to_exclude_join]
+# %%
+df1_columns_to_join = [x for x in df1_columns_to_join if x not in df1_columns_to_exclude_join]
+
+df2_columns_to_join = [x for x in df2_columns_to_join if x not in df2_columns_to_exclude_join]
+# %%
 df_join = df1.merge(right = df2,
-                    left_on = df1.columns.to_list(),
-                    right_on = df2.columns.to_list(),
+                    left_on = df1_columns_to_join,
+                    right_on = df2_columns_to_join,
                     how = 'outer')
 # %%
-records_present_in_df1_not_in_df2 = df_join.loc[df_join[df2.columns.to_list()].isnull().all(axis = 1), df1.columns.to_list()]
+records_present_in_df1_not_in_df2 = df_join.loc[df_join[df2_columns_to_join].isnull().all(axis = 1), df1.columns.to_list()]
 # %%
-records_present_in_df2_not_in_df1 = df_join.loc[df_join[df1.columns.to_list()].isnull().all(axis = 1), df2.columns.to_list()]
+records_present_in_df2_not_in_df1 = df_join.loc[df_join[df1_columns_to_join].isnull().all(axis = 1), df2.columns.to_list()]
 # %%
